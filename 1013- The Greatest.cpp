04c65104944e9199ed41c19b1875d82244619c9d
abdussukkur85 <abdussukkur73@gmail.com>
2020-06-28 17:54:48 +0600

    Beginner Problem

--- 1013- The Greatest.cpp
+++ 1013- The Greatest.cpp
@@ -1,6 +1,6 @@
 
 /**
-1012 - The Greatest
+1013 - The Greatest
 Code written by Abdus Sukkur
 Contact: mail: abdussukkur73@gmail.com
 **/
@@ -12,11 +12,13 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    long int a,b,c,maxab, maxvalue;
+    cin>>a>>b>>c;
 
-
-
+    maxab = (a+b+ abs(a-b))/2;
+    maxvalue = (maxab+c+ abs(maxab-c))/2;
+    cout<<maxvalue<<" eh o maior\n";
     return 0;
 }
-
 
 
